763137d0b60846fb084cd8dc2aa7f9a5705d8dbc
mayaframa25 <22151194@aguascalientes.tecnm.mx>
2026-05-19 07:17:51 -0600

    Seguridad

---
+++ tests/Unit/ExampleTest.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Tests\Unit;
+
+use App\Http\Controllers\OperationsController;
+use PHPUnit\Framework\TestCase;
+
+class ExampleTest extends TestCase
+{
+    /**
+     * A basic test example.
+     */
+    public function test_that_true_is_true(): void
+    {
+        $this->assertTrue(true);
+    }
+
+    public function test_addition_result(): void
+    {
+        $controller = new OperationsController;
+
+        $result = $controller->addition(a: 4, b: 9);
+        $this->assertNotNull($result);
+        $this->assertEquals(13, $result);
+    }
+}
